1ae46886c7086222da59545f6bda20edf1f26fe2
AstolofoBunny <166725995+AstolofoBunny@users.noreply.github.com>
2025-08-02 14:07:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": fileURLToPath(new URL("./client/src", import.meta.url)),
+      "@shared": fileURLToPath(new URL("./shared", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./attached_assets", import.meta.url)),
     },
   },
-  root: path.resolve(import.meta.dirname),
+  root: fileURLToPath(new URL("./client", import.meta.url)),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
+    outDir: fileURLToPath(new URL("./dist", import.meta.url)),
     emptyOutDir: true,
   },
   server: {
